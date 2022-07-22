5be50f8616e4d63dd5e4efca807d1565ab773926
Jean Paul Perea <jeanpaper@gmail.com>
2022-07-21 22:20:58 -0500

    Se suben etiquetas

---
+++ src/lang/en.js
@@ -0,0 +1,146 @@
+exports.en = {
+  verbs: {
+    accept: "Accept",
+    back: "Back",
+    cancel: "Cancel",
+    change_color: "Change color",
+    change_password: "Change password",
+    clear: "Clear",
+    delete: "Delete",
+    login: "Login",
+    logout: "Logout",
+    recover_password: "Recover password",
+    sign_up: "signUp",
+    upload: "Upload",
+    confirm: "Confirm",
+    recognize: "Recognize",
+    save_changes: "Save changes",
+    send_recovery_mail: "Send recovery mail",
+    train: "Train",
+    update: "Update",
+  },
+  labels: {
+    alert: "Alert",
+    no_data: "No data available",
+    my_account: "My account",
+  },
+  forms: {
+    login: {
+      labels: {
+        email_address: "Email",
+        username: "Username",
+        password: "Password",
+        newPassword: "New password",
+        confirmPassword: "Confirm new password",
+      },
+      placeholders: {
+        email_address: "Enter your email",
+        username: "Enter your username",
+        password: "Enter your password",
+        newPassword: "Enter new password",
+        confirmPassword: "Confirm new password",
+      },
+      messages: {
+        on401: "Please, check your credentials and try again.",
+        forgot_password: "Forgot your password?",
+        recover_password: "Recover password",
+        has_no_account: "Don't have an account yet?",
+        changed: "Password changed successfully",
+      },
+    },
+    signUp: {
+      labels: {
+        username: "Username",
+        password: "Password",
+        name: "Name",
+        confirmPassword: "Confirm password",
+        email_address: "Email",
+        country_code: "Country code",
+        phone_number: "Phone number",
+      },
+      placeholders: {
+        username: "Enter your username",
+        password: "Enter your password",
+        name: "Enter your name",
+        confirmPassword: "Confirm password",
+        email_address: "Enter your email",
+        country_code: "Select a code",
+        phone_number: "Enter your phone number",
+      },
+      messages: {
+        has_account: "Have an account?",
+        on400: "Validation error",
+      },
+    },
+    confirmSignUp: {
+      labels: {
+        email_address: "User email",
+        username: "Username",
+        code: "Confirm code",
+        resend_code: "Resend confirm code",
+      },
+      placeholders: {
+        code: "Enter your confirm code",
+      },
+      messages: {
+        resend_code: "Code resent successfully",
+        confirmed: "Confirmed user. You can now log in",
+        on400: "Validation error",
+      },
+    },
+    changePassword: {
+      labels: {
+        email_address: "User email",
+        username: "Username",
+        oldPassword: "Current password",
+        newPassword: "New password",
+        confirmPassword: "Confirm new password",
+      },
+      placeholders: {
+        oldPassword: "Enter current password",
+        newPassword: "Enter new password",
+        confirmPassword: "Confirm new password",
+      },
+      messages: {
+        changed: "Password changed successfully",
+        on400: "Validation error",
+      },
+    },
+    forgotPassword: {
+      labels: {
+        email_address: "User email",
+        username: "Username",
+      },
+      placeholders: {
+        email_address: "Enter your email",
+        username: "Enter username",
+      },
+      messages: {
+        sended: "Mail sended succesfully",
+        on400: "Validation error",
+      },
+    },
+    confirmForgotPassword: {
+      labels: {
+        email_address: "User email",
+        username: "Username",
+        code: "Confirm code",
+        newPassword: "New password",
+        confirmPassword: "Confirm new password",
+        resend_code: "Resend confirm code",
+      },
+      placeholders: {
+        email_address: "Enter your email",
+        username: "Enter your username",
+        code: "Enter your confirm code",
+        newPassword: "Enter new password",
+        confirmPassword: "Confirm new password",
+      },
+      messages: {
+        resend_code: "Code resent successfully",
+        changed: "Password changed successfully",
+        on400: "Validation error",
+      },
+    },
+  },
+};
---
+++ src/lang/es.js
@@ -0,0 +1,146 @@
+exports.es = {
+  verbs: {
+    accept: "Aceptar",
+    back: "Regresar",
+    cancel: "Cancelar",
+    change_color: "Cambiar color",
+    change_password: "Cambiar contraseña",
+    clear: "Limpiar",
+    delete: "Eliminar",
+    login: "Iniciar sesión",
+    logout: "Cerrar sesión",
+    recover_password: "Recuperar contraseña",
+    sign_up: "Registrar",
+    upload: "Subir",
+    confirm: "Confirmar",
+    recognize: "Reconocer",
+    save_changes: "Guardar cambios",
+    send_recovery_mail: "Enviar correo de recuperación",
+    train: "Entrenar",
+    update: "Actualizar",
+  },
+  labels: {
+    alert: "Alerta",
+    my_account: "Mi cuenta",
+    no_data: "No hay datos disponibles",
+  },
+  forms: {
+    login: {
+      labels: {
+        email_address: "Correo",
+        username: "Nombre de usuario",
+        password: "Contraseña",
+        newPassword: "Nueva contraseña",
+        confirmPassword: "Confirmar contraseña",
+      },
+      placeholders: {
+        email_address: "Ingrese su correo",
+        username: "Ingrese su nombre de usuario",
+        password: "Ingrese su contraseña",
+        newPassword: "Ingrese su nueva contraseña",
+        confirmPassword: "Confirmar su nueva contraseña",
+      },
+      messages: {
+        on401: "Por favor revise sus credenciales y vuelva a intentarlo",
+        forgot_password: "¿Olvidó su contraseña?",
+        recover_password: "Recuperar contraseña",
+        has_no_account: "¿No tiene una cuenta aún?",
+        changed: "La contraseña se cambió correctamente",
+      },
+    },
+    signUp: {
+      labels: {
+        username: "Nombre de usuario",
+        password: "Contraseña",
+        confirmPassword: "Confirmar contraseña",
+        name: "Nombre completo",
+        email_address: "Correo",
+        country_code: "Cod. Pais",
+        phone_number: "Teléfono",
+      },
+      placeholders: {
+        username: "Ingrese su nombre de usuario",
+        password: "Escriba su contraseña",
+        confirmPassword: "Confirmar contraseña",
+        name: "Ingrese su nombre completo",
+        email_address: "Escriba su correo",
+        country_code: "Seleccione un código",
+        phone_number: "Ingrese su número telefónico",
+      },
+      messages: {
+        has_account: "¿Ya tiene una cuenta?",
+        on400: "Hay errores de validación",
+      },
+    },
+    confirmSignUp: {
+      labels: {
+        username: "Nombre de usuario",
+        email_address: "Correo de usuario",
+        code: "Código de confirmación",
+        resend_code: "Reenviar código de confirmación",
+      },
+      placeholders: {
+        code: "Ingrese su código de confirmación",
+      },
+      messages: {
+        resend_code: "Código enviado correctamente",
+        confirmed: "Usuario confirmado. Ya puede iniciar sesión",
+        on400: "Hay errores de validación",
+      },
+    },
+    changePassword: {
+      labels: {
+        email_address: "Correo de usuario",
+        username: "Nombre de usuario",
+        oldPassword: "Contraseña actual",
+        newPassword: "Nueva contraseña",
+        confirmPassword: "Confirmar contraseña",
+      },
+      placeholders: {
+        oldPassword: "Ingrese su contraseña actual",
+        newPassword: "Ingrese su nueva contraseña",
+        confirmPassword: "Confirmar su nueva contraseña",
+      },
+      messages: {
+        changed: "La contraseña se cambió correctamente",
+        on400: "Hay errores de validación",
+      },
+    },
+    forgotPassword: {
+      labels: {
+        email_address: "Correo de usuario",
+        username: "Nombre de usuario",
+      },
+      placeholders: {
+        email_address: "Escriba su correo",
+        username: "Ingrese su nombre de usuario",
+      },
+      messages: {
+        sended: "Correo enviado correctamente",
+        on400: "Hay errores de validación",
+      },
+    },
+    confirmForgotPassword: {
+      labels: {
+        email_address: "Correo de usuario",
+        username: "Nombre de usuario",
+        code: "Código de confirmación",
+        newPassword: "Nueva contraseña",
+        confirmPassword: "Confirmar contraseña",
+        resend_code: "Reenviar código de confirmación",
+      },
+      placeholders: {
+        email_address: "Escriba su correo",
+        username: "Ingrese su nombre de usuario",
+        code: "Ingrese su código de confirmación",
+        newPassword: "Ingrese su nueva contraseña",
+        confirmPassword: "Confirmar su nueva contraseña",
+      },
+      messages: {
+        resend_code: "Código enviado correctamente",
+        changed: "La contraseña se cambió correctamente",
+        on400: "Hay errores de validación",
+      },
+    },
+  },
+};
